fb537b5199005ae89d57e45b7c58e34b801a0bf8
Ehan <ethanliang405@gmail.com>
2021-05-31 14:07:53 -0400

    L2 Vue Instance

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+console.log("Connected");
+
+//Create Vue Instance
+new Vue({
+  el: "#vue-app", //controls everything in this
+  data: {
+    name: "Shaun", //{{name}} in html will output this data
+  },
+});
