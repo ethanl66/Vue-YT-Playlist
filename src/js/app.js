b6dbccbe178317d6fa949047940580b8fc96f093
Ehan <ethanliang405@gmail.com>
2021-05-31 14:52:24 -0400

    L6 Event modifiers

--- src/js/app.js
+++ src/js/app.js
@@ -4,15 +4,13 @@ console.log("Connected");
 new Vue({
   el: "#vue-app",
   data: {
-    age: 25, // v-on:click="age++" for click events to change age. Can fire the code inside the quotations.
-    // OR you can have the code from methods: Then we could call the functions without () in the quotes
-    //Shorthand: '@click, @dblclick
+    age: 25,
     x: 0,
     y: 0,
   },
   methods: {
     add: function (inc) {
-      this.age += inc;
+      this.age += inc; // ADDING MODIFIERS v-on:click.once, v-on:click.prevent
     },
     subtract: function (dec) {
       this.age -= dec;
@@ -20,6 +18,9 @@ new Vue({
     updateXY: function (event) {
       this.x = event.offsetX;
       this.y = event.offsetY;
-    }, //pass event through parameter, no need in HTML
+    },
+    click: function () {
+      alert("You clicked");
+    },
   },
 });
