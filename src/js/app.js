c37faaa32036ae2501a6be4a4bac3eb1a4e727c5
Ehan <ethanliang405@gmail.com>
2021-05-31 14:16:04 -0400

    L3 Data and Methods

--- src/js/app.js
+++ src/js/app.js
@@ -5,5 +5,12 @@ new Vue({
   el: "#vue-app", //controls everything in this
   data: {
     name: "Shaun", //{{name}} in html will output this data
+    job: "Ninja",
+  },
+  methods: {
+    greet: function (time) {
+      //this.name; can access all the data in this function
+      return "Good " + time + " " + this.name; //specified afternoon parameter in HTML
+    },
   },
 });
