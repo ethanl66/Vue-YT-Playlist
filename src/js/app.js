571fcaaae0794853beebd4c2caf6610c493f096c
Ehan <ethanliang405@gmail.com>
2021-06-03 16:38:43 -0400

    L11 Conditionals if, else if, show

--- src/js/app.js
+++ src/js/app.js
@@ -4,16 +4,9 @@ console.log("Connected");
 new Vue({
   el: "#vue-app",
   data: {
-    available: false,
-    nearby: false,
+    error: false,
+    success: false,
   },
   methods: {},
-  computed: {
-    compClasses: function () {
-      return {
-        available: this.available,
-        nearby: this.nearby,
-      };
-    },
-  },
+  computed: {},
 });
