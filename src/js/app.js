c5f42fa8d4547ab5a069d0c1071897b4e4ace23e
Ehan <ethanliang405@gmail.com>
2021-06-03 17:21:16 -0400

    L13 Unit 1 Punchbag Game

--- src/js/app.js
+++ src/js/app.js
@@ -4,14 +4,20 @@ console.log("Connected");
 new Vue({
   el: "#vue-app",
   data: {
-    name: "jack",
-    characters: ["Mario", "Luigi", "Yoshi", "Bowser"],
-    ninjas: [
-      { name: "Ryu", age: 25 },
-      { name: "Yoshi", age: 35 },
-      { name: "Ken", age: 55 },
-    ],
+    health: 100,
+    ended: false,
   },
-  methods: {},
+  methods: {
+    punch: function () {
+      this.health -= 10;
+      if (this.health <= 0) {
+        this.ended = true;
+      }
+    },
+    restart: function () {
+      this.health = 100;
+      this.ended = false;
+    },
+  },
   computed: {},
 });
