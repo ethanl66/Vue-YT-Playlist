815b0806a38998d6f8fa94de91b864414345ab55
Ehan <ethanliang405@gmail.com>
2021-06-03 17:03:42 -0400

    L12 Looping with v-for

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,13 @@ console.log("Connected");
 new Vue({
   el: "#vue-app",
   data: {
-    error: false,
-    success: false,
+    name: "jack",
+    characters: ["Mario", "Luigi", "Yoshi", "Bowser"],
+    ninjas: [
+      { name: "Ryu", age: 25 },
+      { name: "Yoshi", age: 35 },
+      { name: "Ken", age: 55 },
+    ],
   },
   methods: {},
   computed: {},
