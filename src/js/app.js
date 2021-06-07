396dfd2337713672bcf8857e107fc3236ad4dd45
Ehan <ethanliang405@gmail.com>
2021-06-07 17:44:43 -0400

    L15 Intro to Components

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,24 @@
 console.log("Connected");
 
-const one = new Vue({
-  el: "#vue-app-one",
-  data: {
-    title: "Vue App One",
-  },
-  methods: {},
-  computed: {
-    greet: function () {
-      return "Hello from app one";
-    },
-  },
-});
-
-const two = new Vue({
-  el: "#vue-app-two",
-  data: {
-    title: "Vue App Two",
+Vue.component("greeting", {
+  template:
+    "<p>I am  {{name}}. <button v-on:click='changeName'>Change name</button></p>",
+  data: function () {
+    return {
+      name: "Yoshi",
+    };
   },
   methods: {
-    changeTitle: function () {
-      one.title = "Title One Has Been Changed";
-    },
-  },
-  computed: {
-    greet: function () {
-      return "Hello from app two";
+    changeName: function () {
+      this.name = "Mario";
     },
   },
 });
 
-two.title = "Changed Title Two from Outside"; //Can be changed from outside the instnace
+new Vue({
+  el: "#vue-app-one",
+});
 
-//NEXT VIDEO: L15 Introduction to Components
-// https://www.youtube.com/watch?v=EAONyIKrZN0&list=PL4cUxeGkcC9gQcYgjhBoeQH7wiAyZNrYa&index=16
-
-//Make new branch
+new Vue({
+  el: "#vue-app-two",
+});
